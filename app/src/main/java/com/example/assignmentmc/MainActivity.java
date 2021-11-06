198ba4bf95ed4344160abb23f6a77b3fd94ed6a4
Sabrina Asim <sabrinaasim2000@gmail.com>
2021-11-06 14:23:34 +0500

    initializing java

--- app/src/main/java/com/example/assignmentmc/MainActivity.java
+++ app/src/main/java/com/example/assignmentmc/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.assignmentmc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button repositaryButton;
+        Button activityButton;
+
+        repositaryButton = (Button) findViewById(R.id.repositarybtn);
+        activityButton = (Button) findViewById(R.id.assignmentbtn);
+
+        repositaryButton.setOnClickListener((View.OnClickListener) this);
+        activityButton.setOnClickListener((View.OnClickListener) this);
+
+        final TextView Rollnotext;
+        final TextView Nametext;
+
+        Rollnotext = findViewById(R.id.rollnotext);
+        Nametext = findViewById(R.id.nametext);
+
+        Rollnotext.setText("BCSF18A509");
+        Nametext.setText("Sabrina Asim");
+
+        Rollnotext.setTextColor(Color.BLACK);
+        Nametext.setTextColor(Color.BLACK);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.repositarybtn:
+
+            case R.id.assignmentbtn:
+        }
+
+
     }
 }
