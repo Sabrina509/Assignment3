5882e059c8fd5c52cca57684103b98f979c7a097
Sabrina Asim <sabrinaasim2000@gmail.com>
2021-11-09 19:45:00 +0500

    java of second page done

--- app/src/main/java/com/example/assignmentmc/assignment3.java
+++ app/src/main/java/com/example/assignmentmc/assignment3.java
@@ -3,13 +3,10 @@ package com.example.assignmentmc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class assignment3 extends AppCompatActivity {
 
@@ -17,5 +14,26 @@ public class assignment3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment3);
+
+        Button practice, quiz;
+        practice= findViewById(R.id.practice);
+        quiz = findViewById(R.id.quiz);
+
+        quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(assignment3.this, Practice.class);
+                startActivity(intent);
+            }
+        });
+
+        practice.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(assignment3.this, Quiz.class);
+                startActivity(intent);
+            }
+        });
     }
 }
